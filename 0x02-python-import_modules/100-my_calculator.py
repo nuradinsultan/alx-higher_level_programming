5eec1f7478d3856936da70f087b9f91baf3212fa
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-03-10 15:42:51 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to import the variable a from the file
+# variable_load_5.py and handles basic operations
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# Import sys module to access argv
+from sys import argv
+
+# Import the functions: add, sub, mul, div
+from main import add, sub, mul, div
+
+operators = ["+", "-", "*", "/"]
+arg_len = len(argv)
+
 if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    operators = ["+", "-", "*", "/"]
-    if argv_count is not 3:
+    if (arg_len - 1) < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.argv[2] not in operators:
+    elif argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] is "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] is "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] is "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        a, b = int(argv[1]), int(argv[3])
+        if argv[2] == "+":
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif argv[2] == "-":
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif argv[2] == "*":
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        else:
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
