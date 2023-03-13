8ccc55efca22688b0e3dc8307854e0c6575ea78e
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-03-13 23:05:47 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print the number of commandline arguments
-# and the list of its arguments
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
-# import sys module to access argv
-from sys import argv
-
-# Determine the length of argv excluding item at index 0
-argv_length = len(argv) - 1
-
-# This code should not run when this file is imported
 if __name__ == "__main__":
-    # Print out different statements based on length
-    if argv_length > 1:
-        print("{:d} arguments:".format(argv_length))
-    elif argv_length == 0:
-        print("{:d} arguments.".format(argv_length))
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    if argv_count is 0:
+        print("{:d} arguments.".format(argv_count))
+    elif argv_count is 1:
+        print("{:d} argument:".format(argv_count))
+        print("{:d}: {:s}".format(index, sys.argv[1]))
     else:
-        print("{:d} argument:".format(argv_length))
+        print("{:d} arguments:".format(argv_count))
+        while index <= argv_count:
+            print("{:d}: {:s}".format(index, sys.argv[index]))
+            index += 1
 
-    # Loop through and print out item and it's index
-    for arg_item in argv:
-        if argv.index(arg_item) == 0:
-            continue
-        print("{:d}: {}".format(argv.index(arg_item), arg_item))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
