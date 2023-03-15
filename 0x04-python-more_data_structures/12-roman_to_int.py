6b4cc0dc6ed932f3bf15303751896d3f808ae4b2
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-03-15 15:27:42 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    for idx in list(a_dictionary.keys()):
-        if a_dictionary[idx] == value:
-            del a_dictionary[idx]
-    return a_dictionary
+def roman_to_int(roman_string):
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    p = 0
+
+    if type(roman_string) is str and roman_string:
+        for c in range(len(roman_string) - 1, -1, -1):
+            if val[roman_string[c]] >= p:
+                res += val[roman_string[c]]
+            else:
+                res -= val[roman_string[c]]
+            p = val[roman_string[c]]
+    return res
+
