25640edfdfe4da94d6352ea7671de86248cf1bcc
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-03-15 17:04:23 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print the result of the addition of all
-# commandline arguments
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
+import sys
 
-# Import sys module to access argv
-from sys import argv
+if __name__ != "__main__":
+    exit()
 
-# Initialize sum to zero
-sum = 0
+argc = len(sys.argv) - 1
 
-# This code should not run when this file is imported
-if __name__ == "__main__":
+i = 0
+result = 0
+for arg in sys.argv:
+    if i != 0:
+        result += int(arg)
+    else:
+        i += 1
+print("{:d}".format(result))
 
-    # Iterate through the arguments
-    for nums in argv:
 
-        # Skip the first argument
-        if argv.index(nums) == 0:
-            continue
-        # Sum all other arguments
-        sum += int(nums)
 
-    # Print out sum
-    print(sum)
+
+
+
+
+
+
+
+
+
+
+
+  
+
+        
+        
+        
+       
+
+
+
