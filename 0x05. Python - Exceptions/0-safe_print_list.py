4e85875016aee9a8fedbbfb90baa441047093493
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-03-30 15:12:06 +0300

    Update 0-safe_print_list.py

--- 0x05. Python - Exceptions/0-safe_print_list.py
+++ 0x05. Python - Exceptions/0-safe_print_list.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+    Returns:
+        The number of elements printed.
     """
-    Prints x elements of a list.
-    All elements must be printed on the same line followed by a new line.
-    You have to use try: / except:
-    You are not allowed to import any module
-    You are not allowed to use len()
-        Args:
-            my_list: list
-            x: number of elements to print.
-        Returns: the real number of elements printed.
-    """
-    count = 0
-    for item in range(x):
+    ret = 0
+    for i in range(x):
         try:
-            print(my_list[item], end='')
+            print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
             break
-        else:
-            count += 1
-    print()
-    return count
+    print("")
+    return (ret)
